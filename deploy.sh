0546f0b79b4018c3be16331f86aa25169c9bd525
Christian Anderskov Petersen     5000 Service Team <chap@dr.dk>
2017-01-30 20:22:30 +0000

    Site Build (30. jan 2017 20:22:29)

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,24 @@
 echo -e "\033[0;32mDeploying new page...\033[0m"
 
 echo -e "\033[0;32mDeleting old folder...\033[0m"
-rm -rf ../canderskov.github.io/
-
-echo -e "\033[0;32mRunning hugo...\033[0m"
-hugo -d ../canderskov.github.io
+rm -rf ../canderskov.github.io
 
 echo -e "\033[0;32mChanging to blog directory...\033[0m"
 cd ../canderskov.github.io
 
+echo -e "\033[0;32mAdding git remote...\033[0m"
+git init
+git remote add origin git@github.com:CAnderskov/canderskov.github.io.git
+git pull 
+
+echo -e "\033[0;32mRunning hugo...\033[0m"
+hugo -d ../canderskov.github.io
+
+
 echo -e "\033[0;32mCommit and push the new build...\033[0m"
+git add --a
 git commit -am "Site Build (`date`)"
-git push
+git push origin master
 
 echo -e "\033[0;32mChange back to hugo soruce...\033[0m"
 cd ../CAnderskov-hugo
